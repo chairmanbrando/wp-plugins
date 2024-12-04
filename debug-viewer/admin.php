0b46ce74de34524527ebfc97a75318c16716e278
chairmanbrando <chairmanbrando@users.noreply.github.com>
2024-12-03 23:48:30 -0500

    Added Debug Viewer

---
+++ debug-viewer/admin.php
@@ -0,0 +1,27 @@
+<style><?= file_get_contents(DFV_DIR . '/admin.css') ?></style>
+
+<div class="wrap">
+    <h1><?= __('Debug File Viewer') ?></h1>
+    <p><?= __(sprintf('Since you have both <code>WP_DEBUG</code> and <code>WP_DEBUG_LOG</code> enabled, some of your debug file is shown below. This is currently limited to last %s lines because because of reasons. You can change this by adding a <code>DFV_MAX_LINES</code> constant to your config file or with the <code>dfv_max_lines</code> filter.', self::$lines)) ?></p>
+    <p><?= __('For your convenience, (a) the output has been scrolled to the bottom if needed to show the most recent items, and (2) lines that contain a fatal or parse error have been highlighted. If you need to see the full file, use the view or download buttons below.') ?></p>
+    <h2><?= self::$filepath ?> (<?= self::$filesize ?>)</h2>
+    <pre id="log"><?= $lines ?></pre>
+    <h2><?= __('Actions') ?></h2>
+    <p><?= __("Note: Some hosts block web access to <code>.log</code> files or at least the default <code>debug.log</code> file. If that's the case on your host, the view and download buttons below won't work. Even if not blocked, the view button may download the file anyway.") ?></p>
+    <a class="button" href="<?= add_query_arg('action', 'dfv-fresh') ?>"><?= __('Start New Debug File') ?></a>
+    <p><?= __('Renames your current debug file using the current <code>time()</code> and starts a fresh one.') ?></p>
+    <a class="button" href="<?= self::$fileurl ?>" target="_blank"><?= __('View Full Debug File') ?></a>
+    <p><?= __('Opens up the full debug file in a new tab.') ?></p>
+    <a class="button" href="<?= self::$fileurl ?>" download><?= __('Download Debug File') ?></a>
+    <p><?= __('Does what it says on the tin.') ?></p>
+    <a href="<?= add_query_arg('action', 'dfv-clean') ?>" class="button"><?= __('Clean Debug File') ?></a>
+    <p><?= __('Remove deprecations, notices, and empty lines from the debug file &mdash; leaving only errors, warnings, and purposeful calls to <code>error_log()</code>. Warning: This is not reversible.') ?></p>
+    <a class="button" href="<?= add_query_arg('action', 'dfv-clear') ?>"><?= __('Empty Debug File') ?></a>
+    <p><?= __('Empties your current debug file. Warning: This is not reversible.') ?></p>
+</div>
+
+<script>
+    jQuery(function ($) {
+        $('#log').scrollTop(9999);
+    });
+</script>
---
+++ debug-viewer/index.php
@@ -0,0 +1,180 @@
+<?php
+
+/**
+ * Plugin Name: Debug File Viewer
+ * Description: Adds a "tool" that displays <code>debug.log</code> in the back end of your site. Why? Because some hosts restrict access to this file, and going through SFTP to view it is a minor pain in the butt.
+ * Version:     1.1.1
+ * Author:      @chairmanbrando
+ * Update URI:  false
+ */
+
+if (! defined('ABSPATH')) exit;
+
+// Nothing to do if they don't have both of these!
+if (! defined('WP_DEBUG') || ! WP_DEBUG) return;
+if (! defined('WP_DEBUG_LOG') || ! WP_DEBUG_LOG) return;
+
+define('DFV_DIR', untrailingslashit(plugin_dir_path(__FILE__)));
+define('DFV_URL', untrailingslashit(plugin_dir_url(__FILE__)));
+
+if (! defined('DFV_MAX_LINES')) {
+    define('DFV_MAX_LINES', 200);
+}
+
+class DebugFileViewer {
+
+    private static $filepath;
+    private static $fileurl;
+    private static $filesize;
+    private static $lines;
+
+    public static function action() {
+        add_action('wp_loaded',  __CLASS__ . '::wp_loaded');
+        add_action('admin_menu', __CLASS__ . '::admin_menu');
+    }
+
+    // ----- @privates -------------------------------------------------------------------------- //
+
+    private static function action_clean() {
+        $remove = [
+            '/^\[.+UTC\] \n/',
+            '/.+auditor:.+\n/',
+            '/.+Deprecated.+\n/',
+            '/.+Notice.+\n/'
+        ];
+
+        $read  = fopen(self::$filepath, 'r');
+        $write = fopen(self::$filepath . '.tmp', 'w');
+
+        while (! feof($read)) {
+            $line = fgets($read);
+            $line = preg_replace($remove, '', $line);
+
+            fputs($write, $line);
+        }
+
+        fclose($read);
+        fclose($write);
+
+        unlink(self::$filepath);
+        rename(self::$filepath . '.tmp', self::$filepath);
+        self::redirect();
+    }
+
+    private static function action_clear() {
+        file_put_contents(self::$filepath, '');
+        self::redirect();
+    }
+
+    // @todo Check if [g]zip capability exists and zip the backup.
+    private static function action_fresh() {
+        $new = (strrpos(self::$filepath, '.') === false)
+             ? sprintf('%s-%s', self::$filepath, time())
+             : str_replace('.', sprintf('-%s.', time()), self::$filepath);
+
+        rename(self::$filepath, $new);
+        self::redirect();
+    }
+
+    private static function file_size() {
+        $bytes  = filesize(self::$filepath);
+        $factor = floor((strlen($bytes) - 1) / 3);
+        $units  = explode(' ', 'B K M G T P');
+
+        return sprintf('%.2f %s', $bytes / pow(1024, $factor), $units[$factor]);
+    }
+
+    private static function file_stats() {
+        self::$filepath = (is_bool(WP_DEBUG_LOG)) ? WP_CONTENT_DIR . '/debug.log' : ABSPATH . WP_DEBUG_LOG;
+        self::$fileurl  = (is_bool(WP_DEBUG_LOG)) ? WP_CONTENT_URL . '/debug.log' : trailingslashit(home_url()) . WP_DEBUG_LOG;
+
+        if (! file_exists(self::$filepath)) {
+            touch(self::$filepath);
+        }
+
+        self::$filesize = self::file_size();
+        self::$lines    = apply_filters('dfv_max_lines', DFV_MAX_LINES);
+    }
+
+    private static function read_lines($num) {
+        $file = new \SplFileObject(self::$filepath, 'r');
+
+        $file->seek(PHP_INT_MAX);
+
+        $lines = new \LimitIterator($file, $file->key() - $num, $file->key());
+        $lines = iterator_to_array($lines);
+        $file  = null;
+
+        return apply_filters('dfv_read_lines', $lines);
+    }
+
+    private static function redirect() {
+        if (wp_redirect(admin_url('tools.php?page=debug-file-viewer'))) {
+            exit;
+        }
+
+        return false;
+
+        // Is this illegal? 🤔
+        return (true) ? exit : false;
+    }
+
+    // ----- @hooks ----------------------------------------------------------------------------- //
+
+    public static function wp_loaded() {
+        if ('tools.php' !== $GLOBALS['pagenow']) return;
+        if (! array_key_exists('page', $_GET)) return;
+        if ('debug-file-viewer' !== $_GET['page']) return;
+
+        self::file_stats();
+
+        if (! array_key_exists('action', $_GET)) return;
+
+        switch ($_GET['action']) {
+            case 'dfv-clean' : self::action_clean(); break;
+            case 'dfv-clear' : self::action_clear(); break;
+            case 'dfv-fresh' : self::action_fresh(); break;
+        }
+    }
+
+    public static function admin_menu() {
+        if (! defined('WP_DEBUG') || ! WP_DEBUG) return;
+        if (! defined('WP_DEBUG_LOG') || ! WP_DEBUG_LOG) return;
+
+        add_submenu_page('tools.php', 'Debug Viewer', 'Debug File Viewer', 'manage_options', 'debug-file-viewer', __CLASS__ . '::admin_page');
+    }
+
+    public static function admin_page() {
+        $lines = self::read_lines(DFV_MAX_LINES);
+
+        $lines = array_map(function ($line) {
+            $line = str_replace('<', '&lt;', $line);
+
+            if (strpos($line, 'Fatal error') !== false || strpos($line, 'Parse error') !== false) {
+                $line = "<mark>{$line}</mark>";
+            }
+
+            return $line;
+        }, $lines);
+
+        $lines = trim(implode('', array_filter($lines)));
+
+        require DFV_DIR . '/admin.php';
+    }
+
+}
+
+DebugFileViewer::action();
+
+// add_action('admin_init', function () {
+//     $message = 'You can uncomment this hook to have some gibberish data drop into your log file each '
+//              . 'time an admin page is accessed. That way you can test the action buttons with ease.';
+//     $cipher  = 'aes-128-gcm';
+//
+//     if (in_array($cipher, openssl_get_cipher_methods())) {
+//         $iv  = openssl_random_pseudo_bytes(openssl_cipher_iv_length($cipher));
+//         $key = openssl_random_pseudo_bytes(32);
+//
+//         error_log(openssl_encrypt($message, $cipher, $key, 0, $iv, $tag));
+//     }
+// });
