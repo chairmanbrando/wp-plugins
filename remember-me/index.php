487ca1f7b95b950cfc3c8d5bf3f817c008385293
chairmanbrando <chairmanbrando@users.noreply.github.com>
2025-05-22 02:33:11 -0400

    ChatGPT cleaned up some bad encoding
    This wasn't showing up in the available plugins list on my own site, and copy-pasting its code from github.com wasn't fixing anything. In lieu of writing it from scratch to try to get around some unknown issue, I asked an LLM...

--- remember-me/index.php
+++ remember-me/index.php
@@ -1,22 +1,22 @@
 <?php
 
 /**
- * Plugin Name: Remember Me
- * Description: I'll never let go, Jack. (Checks the "Remember Me" box on the login screen so you don't have to!)
- * Author: chairmanbrando
- * Author URI: https://chairmanbrando.github.io/
- * Version: 1.0
- * Update URI: false
- * Note: Even if it could, this plugin will never check the stupid "pineapple is delicious on pizza" input on wordpress.com sites. Matt has lost his damned mind.
- * Link: https://en.wikipedia.org/wiki/WP_Engine#WordPress_dispute_and_lawsuit
- */
+ * Plugin Name: Remember Me
+ * Description: I'll never let go, Jack. (Checks the "Remember Me" box on the login screen so you don't have to!)
+ * Author:      chairmanbrando
+ * Author URI:  https://chairmanbrando.github.io/
+ * Version:     1.0
+ * Update URI:  false
+ * Note:        Even if it could, this plugin will never check the stupid "pineapple is delicious on pizza" input on wordpress.com sites. Matt has lost his damned mind.
+ * Link:        https://en.wikipedia.org/wiki/WP_Engine#WordPress_dispute_and_lawsuit
+ */
 
 add_action('login_footer', function () {
-    ?>
+?>
     <script>
-        document.addEventListener('DOMContentLoaded', (e) => {
+        document.addEventListener('DOMContentLoaded', () => {
             document.querySelector('#rememberme').checked = true;
         });
     </script>
-    <?php
+<?php
 });
